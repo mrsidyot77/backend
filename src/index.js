a9fbd9792479ef87db3bad39fb408340ead802df
mrsidyot77 <mrsidyot@gmail.com>
2024-08-08 18:45:41 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 // require('dotenv').config({path: './env'})
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config({path: './env'})
-
+dotenv.config({ path: "./env" });
 
 connectDB()
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Servr is running at PORT: ${process.env.PORT}`);
+      app.on("error", (error) => {
+        console.log("ERROR: db not able to listen express", error);
+      });
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB Connection failed !!!! ", error);
+  });
 
 /*import express from "express";
 
